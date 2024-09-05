6d95a652ab1c22c930be5b8938d1193e9a6e5c92
julioCTufrj <163363164+julioCTufrj@users.noreply.github.com>
2024-09-05 10:29:21 -0300

    Create COMPARATOR_BIT.vhd

---
+++ COMPARATOR_BIT.vhd
@@ -0,0 +1,21 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity COMPARATOR_BIT is
+  Port (A: in STD_LOGIC;
+        B: in STD_LOGIC;
+        equal: out STD_LOGIC;
+        greater_than: out STD_LOGIC;
+        less_than: out STD_LOGIC;
+  );
+  end COMPARATOR_BIT;
+
+  architecture Behavioral of COMPARATOR_BIT is
+
+  begin
+    equal <= not(A xor B);
+    -- A > B
+    greater_than <= A and not(B);
+    -- A < B
+    less_than <= not(A) and B;
+  end Behavioral;
